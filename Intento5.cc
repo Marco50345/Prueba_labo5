d959ae61564fafd4ec37f8e75ede441a547e8209
Marco50345 <00114825@uca.edu.sv>
2025-09-23 23:11:41 -0600

    Quemen la U :V

--- Intento5.cc
+++ Intento5.cc
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout <<"Michelle , ya valio vrga el semestre , me doy de baja. Te quiere don ramon " <<std::endl; 
+    std::cout <<"Michelle , ya valio vrga el semestre , me doy de baja. Te quiere don ramon :(" <<std::endl; 
     return 0;
 }
